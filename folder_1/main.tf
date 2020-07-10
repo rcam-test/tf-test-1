da13ab1da7375e7ad05d2bcb3ef8d9e84d38c76f
Robert Cam <robert.cam@openx.com>
2020-07-10 14:18:01 -0700

    Update workflow and add TF code

---
+++ folder_1/main.tf
@@ -0,0 +1,16 @@
+
+terraform {
+  backend "remote" {
+    organization = "rcam-org"
+
+    workspaces {
+      name = "tf-test-1"
+    }
+  }
+}
+
+resource "null_resource" "terraform-github-actions" {
+  triggers = {
+    value = "This resource was created using GitHub Actions in Folder_1!"
+  }
+}
