bda76c86f456981226f2b88d2044f3112c440d6d
Robert Cam <robert.cam@openx.com>
2020-07-10 15:44:56 -0700

    clean up configs

--- folder_2/main.tf
+++ folder_2/main.tf
@@ -1,17 +1,5 @@
-
-terraform {
-  backend "remote" {
-    hostname     = "app.terraform.io"
-    organization = "rcam-org"
-
-    workspaces {
-      prefix = "tf-test-1"
-    }
-  }
-}
-
 resource "null_resource" "terraform-github-actions" {
   triggers = {
-    value = "This resource was created using GitHub Actions! folder_2"
+    value = "This resource was created using GitHub Actions! folder_2 pr #1"
   }
 }
