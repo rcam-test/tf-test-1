dfbdfc247c117c5a59f97a8dccd4d8003dd0cdfd
Robert Cam <robert.cam@openx.com>
2020-07-11 06:13:28 -0700

    Update to shared_variables.tf to symlink

--- folder_1/shared_variables.tf
+++ folder_1/shared_variables.tf
@@ -1,11 +1,1 @@
-# Contains variables that are shared across the common directory and the customer_domains directory.
-
-variable "gateway_anycast_answers" {
-  description = "Anycast IP addressesses for gateway"
-  default     = ["1.2.3.4", "5.6.7.8"]
-}
-
-variable "ui_answers" {
-  description = "OpenX UI IP address (ui.prod.gcp.openx.org)"
-  default     = ["4.3.2.1"]
-}
+../shared/shared_variables.tf
