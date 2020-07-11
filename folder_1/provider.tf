e8d61a72434587d1ca7fb231230bebf73472aa9b
Robert Cam <robert.cam@openx.com>
2020-07-10 22:51:42 -0700

    Fix provider fields

--- folder_1/provider.tf
+++ folder_1/provider.tf
@@ -1,11 +1,7 @@
 provider "google" {
-  version     = "~> 3.0"
-  credentials = file("~/.gcp_keys/ox-lab-robert-cam-7abbe0af6053.json")
-  project     = "ox-lab-robert-cam"
+  version = "~> 3.0"
 }
 
 provider "google-beta" {
-  version     = "~> 3.0"
-  credentials = file("~/.gcp_keys/ox-lab-robert-cam-7abbe0af6053.json")
-  project     = "ox-lab-robert-cam"
+  version = "~> 3.0"
 }
